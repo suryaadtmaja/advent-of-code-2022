4a5e5283664a7b4f470225a6aef471acf7648f38
sadt <suryaadtmaja13@gmail.com>
2022-12-01 15:41:45 +0700

    add nvmrc

--- day-1/day-1-part-1.js
+++ day-1/day-1-part-1.js
@@ -2,16 +2,21 @@ const fs = require('fs')
 const path = __dirname
 
 fs.readFile(`${path}/input.txt`, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err)
-		return
-	}
+  if (err) {
+    console.error(err)
+    return
+  }
 
-	const reducer = (acc, curr) => {
-		const sum = parseInt(acc) + parseInt(curr)
-		return sum
-	}
+  const reducer = (acc, curr) => {
+    const sum = parseInt(acc) + parseInt(curr)
+    return sum
+  }
 
-	let result = data.split('\n\n').map(data => data.split('\n').map(data => parseInt(data)).reduce(reducer))	
-	console.log(Math.max(...result))
+  let result = data.split('\n\n').map((data) =>
+    data
+      .split('\n')
+      .map((data) => parseInt(data))
+      .reduce(reducer)
+  )
+  console.log(Math.max(...result))
 })
