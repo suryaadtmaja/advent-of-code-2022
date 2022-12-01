3ac24971436cd0fb0f8b3b72a5477f7902fd9fb1
sadt <suryaadtmaja13@gmail.com>
2022-12-01 15:39:51 +0700

    first

---
+++ day-1/day-1-part-1.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const path = __dirname
+
+fs.readFile(`${path}/input.txt`, 'utf-8', (err, data) => {
+	if (err) {
+		console.error(err)
+		return
+	}
+
+	const reducer = (acc, curr) => {
+		const sum = parseInt(acc) + parseInt(curr)
+		return sum
+	}
+
+	let result = data.split('\n\n').map(data => data.split('\n').map(data => parseInt(data)).reduce(reducer))	
+	console.log(Math.max(...result))
+})
---
+++ day-1/day-1-part-2.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = __dirname
+
+fs.readFile(`${path}/input.txt`, 'utf-8', (err, data) => {
+	if (err) {
+		console.error(err)
+		return
+	}
+
+	const reducer = (acc, curr) => {
+		const sum = parseInt(acc) + parseInt(curr)
+		return sum
+	}
+
+	const findThreeHighestNumber = (arr) => {
+		arr = arr.sort((a, b) => b - a)
+		let list = arr.splice(0, 3)
+		console.log(list.reduce(reducer))
+	}
+
+
+
+	let result = data.split('\n\n').map(data => data.split('\n').map(data => parseInt(data)).reduce(reducer))	
+	findThreeHighestNumber(result)
+})
